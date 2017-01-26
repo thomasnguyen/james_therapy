66b0140610c3d01dd8763b5d4eaeada95d1210a1
thomasnguyen <imtoshiru@gmail.com>
2017-01-26 10:33:45 -0800

    added interactivity on header

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+$('.nav-mobile').on('click',function(){
+	$('.nav-mobile_fullscreen').toggle();
+});
+
+$(".window-close").on('click',function(){
+	$('.nav-mobile_fullscreen').toggle();
+	console.log("dfs");
+});
+
